6a8cb2cbbe8664e7f98d6fb4e29af355e4e133ff
Tammy <tammycwong@gmail.com>
2021-05-20 14:56:03 -0400

    finished

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,40 @@
 class EmployeesController < ApplicationController
+    def index
+        @employees = Employee.all
+    end
+
+    def show 
+        @employee = Employee.find(params[:id])
+    end 
+
+    def new
+        @employee = Employee.new
+        @dogs = Dog.all
+        #associate employee with dog
+    end
+
+    def create
+        @employee = Employee.create(employee_params)
+        redirect_to @employee
+    end
+
+    def edit
+        @employee = Employee.find(params[:id])
+        @dogs = Dog.all
+    end
+
+    def update
+        @employee = Employee.find(params[:id])
+        @employee.update(employee_params)
+        redirect_to @employee
+        
+    end
+
+    private
+    
+    def employee_params
+        params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url, :dog_id)
+    end
+
+
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,7 @@
 class Employee < ApplicationRecord
+    belongs_to :dog
+
+def full_name
+    first_name + " " + last_name
+end
 end
